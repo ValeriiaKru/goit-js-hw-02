f7c8bb675947219484f30f5ad393a79f790b9056
Valeriia <157292164+ValeriiaKru@users.noreply.github.com>
2024-04-13 12:13:37 +0300

    commit message

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+'use strict';
+function makeTransaction(quanity, pricePerDroid, customerCredits) {
+    let totalPrice = quanity * pricePerDroid;
+    if (totalPrice > customerCredits) {
+        return `Insufficient funds!`;
+    } else {
+        return `You ordered ${quanity} droids worth ${totalPrice} credits!`;
+    }
+}
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
